029004067179999897c56369ad76f4430fd28e52
nikrogalskiy <60797451+nikrogalskiy@users.noreply.github.com>
2021-05-08 20:34:18 +0300

    08052021

--- js/app.js
+++ js/app.js
@@ -195,7 +195,13 @@ da.init();
 // 	}
 // }
 // //=End Прокрутка при клике=======================================================================================================================================================
-
+const headerButton = document.querySelector('.header__button ');
+const menuBodyNew = document.querySelector('.menu__body');
+if (headerButton) {
+	headerButton.addEventListener("click", e => {
+		menuBodyNew.classList.remove('_active');
+	});
+}
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
